fe5f8ad17358130fea644dc1d89d86c2b3875d3c
cubeplatform-jin <hyunjin.chung@cubeplatform.co.kr>
2023-01-13 14:36:54 +0900

    Mahjong_Score change browser-sync path

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ const concurrently = require('concurrently');
 const upath = require('upath');
 const fs = require('fs');
 
-const browserSyncPath = upath.resolve(upath.dirname(__filename), './node-modules/.bin/browser-sync');
+const browserSyncPath = upath.resolve(upath.dirname(__filename), './browser-sync');
 const sbPath = upath.resolve(upath.dirname(__filename), './scripts/sb-watch.js');
 const apiPath = upath.resolve(upath.dirname(__filename), './dist');
 console.log(browserSyncPath, fs.existsSync(browserSyncPath));
@@ -11,7 +11,7 @@ concurrently([
     { command: `node ${sbPath}`, name: 'SB_WATCH', prefixColor: 'bgBlue.bold' },
     { command: `node ${apiPath}`, name: 'MAHJONG_SCORE_API', prefixColor: 'bgYellow.bold' },
     {
-        command: `browser-sync --port 8001 --reload-delay 2000 --reload-debounce 2000 dist -w --no-online`,
+        command: `"${browserSyncPath}" --port 8001 --reload-delay 2000 --reload-debounce 2000 dist -w --no-online`,
         name: 'SB_BROWSER_SYNC',
         prefixColor: 'bgGreen.bold',
     }
