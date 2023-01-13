6c2255345bc637fcc5ea14c39c0aa0830878e06f
cubeplatform-jin <hyunjin.chung@cubeplatform.co.kr>
2023-01-13 13:17:16 +0900

    Mahjong_Score test API only

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ const browserSyncPath = upath.resolve(upath.dirname(__filename), './node_modules
 
 concurrently([
     // { command: 'node scripts/sb-watch.js', name: 'SB_WATCH', prefixColor: 'bgBlue.bold' },
-    { command: 'node dist', name: 'MAHJONG_SCORE_API', prefixColor: 'bgYellow.bold' },
+    { command: 'node dist/index.js', name: 'MAHJONG_SCORE_API', prefixColor: 'bgYellow.bold' },
     // {
     //     command: `"${browserSyncPath}" --port 8001 --reload-delay 2000 --reload-debounce 2000 dist -w --no-online`,
     //     name: 'SB_BROWSER_SYNC',
